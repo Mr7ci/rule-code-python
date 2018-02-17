74654d31f584338adbb3eb878b6ff463aa7a3a04
Mr7ci <Arnont7ci@gmail.com>
2018-02-17 21:39:40 +0700

    edit all

--- web-send.php
+++ web-send.php
@@ -12,23 +12,24 @@
     //*********************************
  	echo '<p>Hello World</p>'; 
  	$key = new Crypt_RSA();
+ 	//***********************************
 	$ssh = new Net_SSH2('192.168.1.10',22);
-	/****************************
+	//****************************
 
 	// for no rsa auth
 	//*****************************************
-	/*if (!$ssh->login('pi', 'raspberry')) {
+	if (!$ssh->login('pi', 'raspberry')) {
     	exit('Login Failed');
-	}*/
+	}
 	//******************************************
 
 
 	//if rsa exist **use
     //*********************************
-	$key->loadKey(file_get_contents('sshpri.ppk'));
+	/*$key->loadKey(file_get_contents('sshpri.ppk'));
 	if (!$ssh->login('pi', $key)) {
     exit('Login Failed');
-	}
+	}*/
 	//***********************************
 	/*function packet_handler($str)
 	{
@@ -43,7 +44,7 @@
 	/*echo $ssh->exec("echo raspberry | sudo /usr/local/bin/snort -Q -c /etc/snort/snort.conf -i eth0:eth1 | sudo idstools-u2json --snort-conf /etc/snort.conf  --directory /var/log/snort  --prefix snort.u2  --output /var/log/snort/alerts4.json  --follow  -S /etc/snort/sid-msg.map  -G /etc/snort/gen-msg.map  -C /etc/snort/classification.config \n");*/
 	 // run type when you need to input your password with sudo command but this is not secure
 
-	echo $ssh->exec("echo raspberry | sudo systemctl start snort| sudo idstools-u2json --snort-conf /etc/snort.conf  --directory /var/log/snort  --prefix snort.u2  --output /var/log/snort/alerts4.json  --follow  -S /etc/snort/sid-msg.map  -G /etc/snort/gen-msg.map  -C /etc/snort/classification.config | sudo python mongocomplete.py \n")
+	echo $ssh->exec("sh src.sh 145 drop \n")
 	//echo $ssh->exec("echo raspberry | sudo systemctl start snort \n")
  ?> 
  </body>
