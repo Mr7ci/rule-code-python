14e66169331392dd527ff27df88dc711ef616d82
Mr7ci <Arnont7ci@gmail.com>
2018-02-17 18:51:51 +0700

    update delete rule

--- delete.py
+++ delete.py
@@ -1,9 +1,16 @@
 import os
 import sys
-with open("test.rules","r+") as f:
-    new_f = f.readlines()
-    f.seek(0)
-    for num,line in enumerate(new_f,1):
-        if "567" not in line:
-            f.write(line)
-    f.truncate()
+
+def deleterule(ssid):
+	with open("test.rules","r+") as f:
+	    new_f = f.readlines()
+	    f.seek(0)
+	    for num,line in enumerate(new_f,1):
+		if ssid not in line:
+		    f.write(line)
+	    f.truncate()
+
+if __name__ == "__main__":
+	ssid = str(sys.argv[1])
+	deleterule(ssid)
+	
