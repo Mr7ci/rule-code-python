49ac12aaade230c8af41192a77e8b6fec20f5c97
Mr7ci <Arnont7ci@gmail.com>
2018-02-24 18:24:44 +0700

    add more

--- add_rule.py
+++ add_rule.py
@@ -4,10 +4,21 @@ import sys
 import fileinput as fileinput
 
 def addrule(rule):
-    with open('/etc/snort/rules/test.rules', 'a') as file:
-        file.write(rule)
+	#with open('/etc/snort/rules/test.rules', 'a') as file:
+	#	file.write(rule)
+	with open('/etc/snort/rules/test.rules', "r+U") as f:
+	    try:
+		f.seek(-1, 2)
+		while f.read(1) == "\n":
+		    f.seek(-2, 1)      
+		                       
+	    except IOError:            
+		f.seek(0)              
+	    else:
+		f.write("\n")          
+	    f.write(rule+"\n")
 
 if __name__ == "__main__":
-   rule = input("rule:")
-   #rule = str(sys.argv[1])
+   #rule = input("rule:")
+   rule = str(sys.argv[1])
    addrule(rule)
